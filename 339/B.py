8a15527c0cace2a44c3aea88eab5c898bef79336
morishita <93669927+Durey-creater@users.noreply.github.com>
2024-02-17 18:39:31 +0900

    vectorとprintの修正

--- 339/B.py
+++ 339/B.py
@@ -3,8 +3,8 @@
 # h, w, nをinputで受け取る。splitで空白ごとに分割、
 h, w, n = map(int, input().split())
 
-# 右、左、上、下へ移動するためのリストを生成
-vector =[(0,1),(1,0),(0,-1),(-1,0)]
+# 上、右、下、左へ移動するためのリストを生成
+vector =[(0,-1),(1,0),(0,1),(-1,0)]
 
 # 初期状態のgridを生成
 # ”.”をw個for文でリストに追加し、そのリストをh個for文でリストに追加
@@ -16,21 +16,26 @@ grid = [['.' for _ in range(w)] for _ in range(h)]
 #     for _ in range(w):
 #         row.append('.')
 
-# 初期座標をx, にそれぞれ格納
+# 初期座標をx, yに、向いている方向(vector)の初期情報をvに代入
 x, y, v = 0, 0, 0
 for i in range(n):
+    # 座標のが”.”の場合
     if grid[x][y] == '.':
+        # ”#”に変更
         grid[x][y] = '#'
+        # 向いている方向を変更
         v = (v + 1) % 4
-        x += vector[v][0]
-        y += vector[v][1]
-    else:
+    else: 
         grid[x][y] = '.'
         v = (v - 1) % 4
-        x += vector[v][0]
-        y += vector[v][1]
-    x = (x + vector[v][0]) % h
-    y = (y + vector[v][1]) % w
     
-print(grid)
+    #vx, vyに座標の移動量を代入
+    vx, vy = vector[v]
+    # 移動量を足して、h, wで割った余りを代入(トーラス状にするため)
+    x = (x + vx) % h
+    y = (y + vy) % w
 
+# gridリストの各行をfor文で出力
+# *で要素を展開して、、sepで空白なしで出力する。
+for j in grid:
+    print(*j, sep='')
