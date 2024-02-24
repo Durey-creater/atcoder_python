c118b8391d75c1ae08c77b81312d98ec433121ca
morishita <93669927+Durey-creater@users.noreply.github.com>
2024-02-24 16:18:05 +0900

    338Bの追加、340Cの挑戦

---
+++ 340/C.py
@@ -0,0 +1,10 @@
+n = int(input())
+blackboard = []
+blackboard.append(n)
+blackboard = sorted(blackboard)
+
+
+while any(n >= 2 for n in blackboard)
+
+# for i in range(1, len(blackboard)):
+#     if blackbord[i] >= 2:
