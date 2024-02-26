f8d7144b03213858f53f97c6ba7bf49de0316767
morishita <93669927+Durey-creater@users.noreply.github.com>
2024-02-26 13:44:49 +0900

    まだ実行時間制限超過

--- 340/C.py
+++ 340/C.py
@@ -7,20 +7,28 @@ cost = 0
 # blackboard = sorted(blackboard)
 
 
-while any(i >= 2 for i in blackboard):
-    for i in range(len(blackboard)):
-        if blackboard[i] >= 2:
-            x = blackboard[i]
-            cost += x
-            del blackboard[i]
-            blackboard.append(math.floor(x / 2))
-            blackboard.append(math.ceil(x / 2))
-            blackboard.sort()
+while blackboard:
+    max_blackboard = max(blackboard)
+    if max_blackboard >= 2:
+        x = max_blackboard
+        cost += x
+        blackboard.append(math.floor(x / 2))
+        blackboard.append(math.ceil(x / 2))
+        blackboard.remove(x)
+    if max_blackboard < 2:
+        break
 
 print(cost)
 
+#    for i in range(len(blackboard)):
+#         if blackboard[i] >= 2:
+#             x = blackboard[i]
+#             cost += x
+#             del blackboard[i]
+#             blackboard.append(math.floor(x / 2))
+#             blackboard.append(math.ceil(x / 2))
+#             # blackboard.sort()
+
+# print(cost)
 
 
-# わからん。
-# for i in range(1, len(blackboard)):
-#     if blackbord[i] >= 2:
